17cb9791f15f01f77f7f13477fc2dda53485d1f6
tolu <toluokupe@yahoo.com>
2022-01-23 23:37:23 -0600

    Completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+function introduction(name = "Sarah") {
+    return (`Hi, my name is ${name}.`);
+  }
+function introductionWithLanguage(name ="Sarah", language = "Emer.js") {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+const name = "Theressa"
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
+}
